0f2829396d298877906c71629b9948b91c3edfcb
Earl Kennedy <mnenmenth@gmail.com>
2020-09-01 13:25:59 -0400

    Add TodoItem service and it's implementation

---
+++ src/main/java/io/github/mnenmenth/todoapp/service/TodoItemService.java
@@ -0,0 +1,20 @@
+package io.github.mnenmenth.todoapp.service;
+
+import io.github.mnenmenth.todoapp.db.TodoItem;
+
+import java.util.Collection;
+
+/**
+ * Created by Earl Kennedy
+ * https://github.com/Mnenmenth
+ */
+
+// Service to perform operations on TodoItems
+public interface TodoItemService
+{
+    TodoItem createNew(String name, String description);
+    TodoItem delete(Integer id);
+    TodoItem findById(Integer id);
+    Collection<TodoItem> findByName(Integer id);
+    Collection<TodoItem> findAll();
+}
