133ae05f744f2be6b7981f2c8ecd53925607d308
Earl Kennedy <mnenmenth@gmail.com>
2020-09-01 21:33:31 -0400

    Fix not being able to run all tests at once
    This adds the DirtiesContext annotation to each test class and configures it to clear the context cache between each test method. This also fixes the new todo item test checking for a status code of OK instead of CREATED

--- src/test/java/io/github/mnenmenth/todoapp/controllers/DeleteTodoItemControllerTest.java
+++ src/test/java/io/github/mnenmenth/todoapp/controllers/DeleteTodoItemControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class DeleteTodoItemControllerTest
 {
     @Autowired
