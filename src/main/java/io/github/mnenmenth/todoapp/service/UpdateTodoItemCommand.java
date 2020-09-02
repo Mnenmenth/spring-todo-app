6922492ffac7c7888eb263557a705bf1c84ed210
Earl Kennedy <mnenmenth@gmail.com>
2020-09-01 20:38:50 -0400

    Update everything to use new exception

--- src/main/java/io/github/mnenmenth/todoapp/service/UpdateTodoItemCommand.java
+++ src/main/java/io/github/mnenmenth/todoapp/service/UpdateTodoItemCommand.java
@@ -2,6 +2,7 @@ package io.github.mnenmenth.todoapp.service;
 
 import io.github.mnenmenth.todoapp.db.TodoItem;
 import io.github.mnenmenth.todoapp.db.TodoRepository;
+import io.github.mnenmenth.todoapp.service.exception.TodoItemNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -53,7 +54,7 @@ public class UpdateTodoItemCommand implements IServiceCommand
         // Otherwise, return a bad request status with error message
         else
         {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No TodoItem entity exists with the given id of '" + id + "'");
+            throw new TodoItemNotFoundException(id);
         }
     }
 }
